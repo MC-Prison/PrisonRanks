be27b940f90290aa6ca434766adfea91828f924f
SirFaizdat <fdatoo7@gmail.com>
2017-01-21 16:20:09 -0500

    Incomplete rank ladder

--- src/main/java/tech/mcprison/prison/ranks/RankLadder.java
+++ src/main/java/tech/mcprison/prison/ranks/RankLadder.java
@@ -17,6 +17,11 @@
 
 package tech.mcprison.prison.ranks;
 
+import tech.mcprison.prison.store.AbstractJsonable;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 /**
  * A certain sequence that rank-ups will follow. There may be multiple
  * rank ladders on the server at a time, and one rank may be a part of
@@ -24,5 +29,69 @@ package tech.mcprison.prison.ranks;
  *
  * @author Faizaan A. Datoo
  */
-public class RankLadder {
+public class RankLadder extends AbstractJsonable<RankLadder> {
+
+    /*
+     * Fields & Constants
+     */
+
+    private String name;
+    private Map<Integer, Rank> ranks;
+
+    /*
+     * Constructor
+     */
+
+    /**
+     * For serialization purposes only.
+     */
+    public RankLadder() {
+    }
+
+    /**
+     * Initialize a new {@link RankLadder}.
+     *
+     * @param name The name of this rank ladder; this is how it will be identified. This will be made lowercase.
+     */
+    public RankLadder(String name) {
+        this.name = name.toLowerCase();
+        this.ranks = new LinkedHashMap<>();
+    }
+
+    /*
+     * Methods
+     */
+
+    public void addRank(int position, Rank rank) {
+        ranks.put(position, rank);
+    }
+
+    public void removeRank(int position) {
+        if (ranks.size() < position) {
+            throw new ArrayIndexOutOfBoundsException(
+                position + " is greater than array size of " + ranks.size());
+        }
+
+        ranks.remove(position);
+
+        int i = position + 1;
+
+        while(i <= ranks.size()) {
+            // TODO Finish this
+        }
+
+    }
+
+    /*
+     * Getters & Setters
+     */
+
+    public String getName() {
+        return name;
+    }
+
+    public Map<Integer, Rank> getRanks() {
+        return ranks;
+    }
+
 }
