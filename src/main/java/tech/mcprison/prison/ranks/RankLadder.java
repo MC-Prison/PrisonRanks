8dc98f7ae6b0e664235ea6962a9822d15a0a9535
SirFaizdat <fdatoo7@gmail.com>
2017-01-23 21:08:39 -0500

    Progress on ladders

--- src/main/java/tech/mcprison/prison/ranks/RankLadder.java
+++ src/main/java/tech/mcprison/prison/ranks/RankLadder.java
@@ -21,6 +21,7 @@ import tech.mcprison.prison.store.AbstractJsonable;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 /**
  * A certain sequence that rank-ups will follow. There may be multiple
@@ -62,10 +63,32 @@ public class RankLadder extends AbstractJsonable<RankLadder> {
      * Methods
      */
 
+    /**
+     * Add a rank to this ladder.
+     *
+     * @param position The place in line to put this rank, beginning at 0. The player will
+     *                 be taken through each rank by order of their positions in the ladder.
+     * @param rank     The {@link Rank} to add.
+     */
     public void addRank(int position, Rank rank) {
         ranks.put(position, rank);
     }
 
+    /**
+     * Add a rank to this ladder. The rank's position will be set to the next available position (i.e. at the end of the ladder).
+     *
+     * @param rank The {@link Rank} to add.
+     */
+    public void addRank(Rank rank) {
+        ranks.put(getNextAvailablePosition(), rank);
+    }
+
+    /**
+     * Removes a rank from this ladder.
+     *
+     * @param position The position of the rank to be removed. The positions of the rest of the
+     *                 ranks will be downshifted to fill the gap.
+     */
     public void removeRank(int position) {
         if (ranks.size() < position) {
             throw new ArrayIndexOutOfBoundsException(
@@ -97,6 +120,51 @@ public class RankLadder extends AbstractJsonable<RankLadder> {
 
     public Map<Integer, Rank> getRanks() {
         return ranks;
+    }
+
+    /**
+     * Returns the next highest rank in the ladder.
+     *
+     * @param oldPosition The position of the current rank.
+     * @return An optional containing either the rank if there is a next rank in the ladder, or empty if there isn't.
+     */
+    public Optional<Rank> getNext(int oldPosition) {
+
+        for (Map.Entry<Integer, Rank> rankEntry : ranks.entrySet()) {
+            if (rankEntry.getKey() >= oldPosition) {
+                return Optional.of(rankEntry.getValue());
+            }
+        }
+
+        return Optional.empty();
+    }
+
+    public Optional<Rank> getPrevious(int oldPosition) {
+
+        for (int position = oldPosition - 1; position >= 0; position--) {
+            if (ranks.containsKey(position)) {
+                return Optional.of(ranks.get(position));
+            }
+        }
+
+        return Optional.empty();
+    }
+
+    /**
+     * Returns the next available position for a rank, by finding the highest one.
+     *
+     * @return The open position.
+     */
+    private int getNextAvailablePosition() {
+        int highest = 0;
+
+        for (int position : ranks.keySet()) {
+            if (position >= highest) {
+                highest = position;
+            }
+        }
+
+        return highest;
     }
 
 }
